cd87b623a4f99ffbf9d4d69871a17906720e5636
sebastian-anandas <sebastian.anandas@gmail.com>
2025-02-07 17:34:38 +0530

    Spring OAuth2 implementation

---
+++ src/main/java/io/seb/userservice/security/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package io.seb.userservice.security.repositories;
+
+import java.util.Optional;
+
+import io.seb.userservice.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
